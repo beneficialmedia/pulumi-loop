83ec9767cd74d4cfea4f6a76047b31af15c7e895
dylankil <dylan@pulumi.com>
2024-07-19 11:50:31 -0700

    Edit firebase config

--- ratecraig/src/firebase/index.js
+++ ratecraig/src/firebase/index.js
@@ -19,9 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-// export const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 
 export default app;
