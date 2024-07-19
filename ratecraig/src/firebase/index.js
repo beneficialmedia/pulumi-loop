1bc4d6d1a2587b83458512277d4f98fc5de1e6ff
dylankil <dylan@pulumi.com>
2024-07-19 12:24:06 -0700

    Update to use Pulumi env vars

--- ratecraig/src/firebase/index.js
+++ ratecraig/src/firebase/index.js
@@ -1,6 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import * as pulumi from "@pulumi/pulumi";
+
+const config = new pulumi.Config();
+
+const apiKey = config.requireSecret("NEXT_PUBLIC_FIREBASE_API_KEY");
+const authDomain = config.requireSecret("NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN");
+const projectId = config.requireSecret("NEXT_PUBLIC_FIREBASE_PROJECT_ID");
+const storageBucket = config.requireSecret("NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET");
+const messagingSenderId = config.requireSecret("NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID");
+const appId = config.requireSecret("NEXT_PUBLIC_FIREBASE_APP_ID");
+const measurementId = config.requireSecret("NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID");
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +19,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
 
 // Initialize Firebase
